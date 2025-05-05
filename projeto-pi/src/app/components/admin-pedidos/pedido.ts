3870aa7e2cbde1fa70eb35228717796d03852f46
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2025-05-05 02:26:30 -0300

    add projeto and json-server

---
+++ projeto-pi/src/app/components/admin-pedidos/pedido.ts
@@ -0,0 +1,23 @@
+export interface Pedido {
+    id: number;
+    tipo_pedido: string;
+    forma_pagamento: string;
+    valor_total: number;
+    status: string;
+    observacoes: string;
+    cliente_id: number;
+    created_at: string;
+    updated_at: string;
+}
+
+
+export interface Pedidopayload {
+    tipo_pedido: string;
+    forma_pagamento: string;
+    valor_total: number;
+    status: string;
+    observacoes: string;
+    cliente_id: number;
+}
+
+
